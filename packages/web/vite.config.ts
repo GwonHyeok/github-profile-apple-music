f1f3dd6af697a207828ed6d7ea9a1956537a2de2
GwonHyeok <me@ghyeok.io>
2022-07-08 14:48:39 +0900

    [web] inject adsense script from env variable

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
+interface InjectOptionData {
+  adSenseScript: string;
+}
+
+const injectOptionData: InjectOptionData = { adSenseScript: '' };
+
+const generateAdsenseScript = (clientId: string) =>
+  `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${clientId}" crossorigin="anonymous"></script>`;
+
+// Adsense Script
+if (process.env.VITE_ADSENSE_ENABLED === 'true' && process.env.VITE_ADSENSE_CLIENT_ID) {
+  injectOptionData.adSenseScript = generateAdsenseScript(process.env.VITE_ADSENSE_CLIENT_ID);
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: injectOptionData,
+      },
+    }),
+  ],
 });
