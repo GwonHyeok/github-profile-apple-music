7516ae4c201de5c8e7b71e19af305f8b62c0228f
GwonHyeok <me@ghyeok.io>
2022-12-06 17:57:45 +0900

    [web] use firebase emulator when running in dev mode

--- packages/web/src/firebase.ts
+++ packages/web/src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,3 +15,8 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const firestore = getFirestore(app);
+
+// 로컬 개발 환경에서 Emulator 사용
+if (import.meta.env.DEV) {
+  connectFirestoreEmulator(firestore, 'localhost', 8080);
+}
